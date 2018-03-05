d95d588936252bed8c529c333f775033d4231074
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2018-03-05 16:06:00 -0300

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,19 @@
+from django.urls import resolve
+'''
+resolve é a função que Django utiliza internamente para resolver URLs
+e descobrir para qual função de view eles devem ser mapeados.
+'''
 from django.test import TestCase
 
-class SmokeTest(TestCase):
+from lists.views import home_page
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        '''
+        Estamos verificando se resolve, quando é chamado
+        com '/', que é a raiz do site, encontra uma função
+        chamada home_page.
+        '''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
